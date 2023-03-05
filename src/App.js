ca26b698b0cc7b6c4e7c10680d6d886f31c14493
Shobhit51 <111689849+Shobhit51@users.noreply.github.com>
2023-03-05 22:34:44 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Intro from "./components/intro/Intro";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>
+        <Intro />
+      </h1>
+    </div>
+  );
+}
